27ffc423c3cf4564f66428d6949aa7c7c9f56947
Ashutosh Kumar Bharti <ashutosh.bharti5247@gmail.com>
2024-11-04 13:20:48 +0530

    route controller

--- post-producer/src/routes/post.route.ts
+++ post-producer/src/routes/post.route.ts
@@ -1,16 +1,21 @@
 import { Router, Request, Response } from "express";
 import { z } from "zod";
 import postSchema from "../models/post.model";
+import kafkaConfig from "../config/kafka.config";
 
 const postRouter = Router();
 
 postRouter.post("/create-post", async (req: Request, res: Response) => {
+  try {
     const valid = postSchema.parse(req.body);
-    try {
-        const {title, body} = req.body
-        res.json({})
-    } catch (error) {
-        
-    }
-
+    const { title, body } = req.body;
+    await kafkaConfig.sentToTopic("post", JSON.stringify({ title, body }));
+    console.log('body',req.body);
+    res.status(200).json({ message:"success" });
+  } catch (error) {
+    console.log('create-post-error',error)
+    res.status(403).json({ error });
+  }
 });
+
+export default postRouter
