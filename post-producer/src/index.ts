cfdaf786af668c62a061b337c60b6a475b95a045
Ashutosh Kumar Bharti <ashutosh.bharti5247@gmail.com>
2024-11-04 13:20:33 +0530

    body parser

--- post-producer/src/index.ts
+++ post-producer/src/index.ts
@@ -1,13 +1,15 @@
 import express, { Request, Response } from "express";
 import { init } from "./config/start.services";
 import ip from "ip";
+import postRouter from "./routes/post.route";
 
 const app = express();
-
+app.use(express.json())
 init();
 app.get("/", async (rq: Request, rs: Response) => {
   rs.json({ status: "ok" });
 });
+app.use(postRouter)
 app.listen(3000, () => {
   console.log("listening to 3000");
   console.log("ip=>", ip.address());
